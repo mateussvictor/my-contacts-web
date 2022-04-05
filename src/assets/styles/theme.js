84865ebb9003585693760f53251ae156b036799e
Mateus Victor <mateusvictor.dev@gmail.com>
2022-04-05 16:17:06 -0300

    feat: add theme

---
+++ src/assets/styles/theme.js
@@ -0,0 +1,51 @@
+export default {
+  font: {
+    family:
+      "Sora, -apple-system, BlinkMacSystemFont, 'Segoe UI', Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    light: 300,
+    normal: 400,
+    bold: 600,
+    sizes: {
+      xsmall: '1.2rem',
+      small: '1.4rem',
+      medium: '1.6rem',
+      large: '1.8rem',
+      xlarge: '2.0rem',
+      xxlarge: '2.8rem'
+    }
+  },
+
+  colors: {
+    primary: '#5061FC',
+    primaryLight: '#6674F4',
+    primaryDark: '#3346F0',
+
+    danger: '#FC5050',
+    dangerLight: '#F97171',
+    dangerDark: '#F63131',
+
+    gray: '##8F8F8F',
+    grayLight: '#E6E6E6',
+    grayDark: '#222222',
+
+    white: '#FFF',
+    black: '#030517',
+    mainBg: '#F6F5FC'
+  },
+
+  spacings: {
+    xxsmall: '0.8rem',
+    xsmall: '1.6rem',
+    small: '2.4rem',
+    medium: '3.2rem',
+    large: '4.0rem',
+    xlarge: '4.8rem',
+    xxlarge: '5.6rem'
+  },
+
+  borderRadius: {
+    small: '0.4rem',
+    medium: '1.6rem',
+    large: '2.4rem'
+  }
+}
