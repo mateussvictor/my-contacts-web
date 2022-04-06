5dc3c126fc082775ef46c43bc8562e6d11a48ca8
Mateus Victor <mateusvictor.dev@gmail.com>
2022-04-06 12:23:11 -0300

    feat: add routes

---
+++ src/Routes.js
@@ -0,0 +1,15 @@
+import { Switch, Route } from 'react-router-dom'
+
+import { Home } from './pages/Home'
+import { NewContact } from './pages/NewContact'
+import { EditContact } from './pages/EditContact'
+
+export default function Routes () {
+  return (
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/new" component={NewContact} />
+      <Route path="/edit/:id" component={EditContact} />
+    </Switch>
+  )
+}
