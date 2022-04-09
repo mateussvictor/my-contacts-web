8c4b93716f75726b9cb19640bc41736563620898
Mateus Victor <mateusvictor.dev@gmail.com>
2022-04-09 19:54:02 -0300

    feat: add Input component

---
+++ src/components/Input.js
@@ -0,0 +1,18 @@
+import styled from 'styled-components'
+
+export const Input = styled.input`
+  width: 100%;
+  background: ${({ theme }) => theme.colors.white};
+  box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.04);
+  border: 2px solid transparent;
+  height: 5.2rem;
+  border-radius: ${({ theme }) => theme.borderRadius.small};
+  outline: none;
+  padding: 0 ${({ theme }) => theme.spacings.xsmall};
+
+  transition: border-color 100ms ease-in;
+
+  &:focus {
+    border: 2px solid ${({ theme }) => theme.colors.primary}
+  }
+`
