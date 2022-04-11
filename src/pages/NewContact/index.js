6da2fa3ad0c0185d4bdffd1f8bfaccda0c7fd57c
Mateus Victor <mateusvictor.dev@gmail.com>
2022-04-11 15:15:36 -0300

    feat: add new contact page

---
+++ src/pages/NewContact/index.js
@@ -0,0 +1,17 @@
+
+import { PageHeader } from '../../components/PageHeader'
+import { ContactForm } from '../../components/ContactForm'
+
+function NewContact () {
+  return (
+    <>
+      <PageHeader title="New Contact" />
+
+      <ContactForm
+        buttonLabel="Save Contact"
+      />
+    </>
+  )
+}
+
+export { NewContact }
