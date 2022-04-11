ecb9271718632ad08b47a89edec64f81658bb43e
Mateus Victor <mateusvictor.dev@gmail.com>
2022-04-11 15:19:39 -0300

    feat: add Router

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,22 +1,26 @@
 import { ThemeProvider } from 'styled-components'
-
+import { BrowserRouter as Router } from 'react-router-dom'
 import theme from '../../assets/styles/theme'
 import Global from '../../assets/styles/global'
 
 import { Header } from '../Header'
-import { ContactsList } from '../ContactsList'
+
+import Routes from '../../Routes'
 
 import * as S from './styles'
 
 function App () {
   return (
-    <ThemeProvider theme={theme}>
-      <Global />
-      <S.Container>
-        <Header />
-        <ContactsList />
-      </S.Container>
-    </ ThemeProvider>
+    <Router>
+      <ThemeProvider theme={theme}>
+        <Global />
+
+        <S.Container>
+          <Header />
+          <Routes />
+        </S.Container>
+      </ ThemeProvider>
+    </Router>
   )
 }
 
