4e85376499187f5c3e1d0ec9a08f31361e1d8654
Mateus Victor <mateusvictor.dev@gmail.com>
2022-06-10 09:11:14 -0300

    feat: set style to small tag

--- src/components/FormGroup/styles.js
+++ src/components/FormGroup/styles.js
@@ -4,4 +4,11 @@ export const Container = styled.div`
   & + & {
     margin-top: 1.6rem;
   }
+
+  small {
+    color: ${({ theme }) => theme.colors.danger.normal};
+    font-size: 1.2rem;
+    margin-top: .8rem;
+    display: block;
+  }
 `
