33222718001d966c1cc7af891a5c8a3bd38302ef
Mateus Victor <mateusvictor.dev@gmail.com>
2022-06-20 17:47:12 -0300

    feat: add func to validate phone number

---
+++ src/utils/formatPhone.js
@@ -0,0 +1,6 @@
+export default function formatPhone (phoneNumber) {
+  return phoneNumber
+    .replace(/\D/g, '')
+    .replace(/^(\d{2})\B/, '($1) ')
+    .replace(/(\d{1})?(\d{4})(\d{4})/, '$1$2-$3')
+}
