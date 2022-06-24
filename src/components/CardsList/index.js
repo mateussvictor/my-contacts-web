576a1a872bfe38df4ea1e76f709570bc2d3807c9
Mateus Victor <mateusvictor.dev@gmail.com>
2022-06-24 17:16:29 -0300

    refactor: add dynamic data from API

--- src/components/CardsList/index.js
+++ src/components/CardsList/index.js
@@ -1,37 +1,55 @@
 import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+import formatPhone from '../../utils/formatPhone'
 
 import { ReactComponent as EditIcon } from '../../assets/icons/edit.svg'
 import { ReactComponent as TrashIcon } from '../../assets/icons/trash.svg'
 
 import * as S from './styles'
 
-function CardsList () {
-  return (
-    <S.CardsContainer>
-      <S.Card>
-        <S.CardItem>
-          <S.CardInfo>
-            <S.ContactName>
-              Mateus Victor
-              <small>Category</small>
-            </S.ContactName>
-            <S.ContactEmail>mateusvictor.dev@gmail.com</S.ContactEmail>
-            <S.ContactPhone>(61) 99929-9109</S.ContactPhone>
-          </S.CardInfo>
+function CardsList ({ contacts }) {
+  const contactsExists = contacts.length > 0
 
-          <S.CardActionsBox>
-            <Link to="/edit/123">
-              <EditIcon />
-            </Link>
+  if (contactsExists) {
+    return (
+      <S.CardsContainer>
+        <S.Card>
+          {contacts.map(contact => (
+            <S.CardItem key={contact.name}>
+              <S.CardInfo>
+                <S.ContactName>
+                  {contact.name}
 
-            <button type="button">
-              <TrashIcon />
-            </button>
-          </S.CardActionsBox>
-        </S.CardItem>
-      </S.Card>
-    </S.CardsContainer>
-  )
+                  {contact.category_name && (
+                    <S.CategoryLabel>{contact.category_name}</S.CategoryLabel>
+                  )}
+                </S.ContactName>
+
+                <S.ContactEmail>{contact.email}</S.ContactEmail>
+
+                <S.ContactPhone>{formatPhone(contact.phone)}</S.ContactPhone>
+              </S.CardInfo>
+
+              <S.CardActionsBox>
+                <Link to={`/edit/${contact.id}`}>
+                  <EditIcon />
+                </Link>
+
+                <button type="button">
+                  <TrashIcon />
+                </button>
+              </S.CardActionsBox>
+            </S.CardItem>
+          ))}
+        </S.Card>
+      </S.CardsContainer>
+    )
+  }
 }
 
 export { CardsList }
+
+CardsList.propTypes = {
+  contacts: PropTypes.array.isRequired
+}
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,37 +1,89 @@
+import { useState, useEffect, useMemo } from 'react'
 import { Link } from 'react-router-dom'
 
 import arrow from '../../assets/icons/arrow.svg'
 
 import { CardsList } from '../../components/CardsList'
-import { Modal } from '../../components/Modal'
+import { Loader } from '../../components/Loader'
+
+import delay from '../../utils/delay'
 
 import * as S from './styles'
 
 function Home () {
+  const [contacts, setContacts] = useState([])
+  const [orderBy, setOrderBy] = useState('asc')
+  const [searchTerm, setSearchTerm] = useState('')
+  const [isLoading, setIsLoading] = useState(true)
+
+  const filteredContacts = useMemo(() => {
+    return contacts.filter(contact => (
+      contact.name.toLowerCase().includes(searchTerm.toLowerCase())
+    ))
+  }, [contacts, searchTerm])
+
+  useEffect(() => {
+    setIsLoading(true)
+
+    async function getContacts () {
+      try {
+        await delay(500)
+
+        const url = `http://localhost:3001/contacts?orderBy=${orderBy}`
+        const response = await fetch(url)
+        const json = await response.json()
+
+        setContacts(json)
+      } catch (error) {
+        throw new Error()
+      } finally {
+        setIsLoading(false)
+      }
+    }
+
+    getContacts()
+  }, [orderBy])
+
+  function handleToggleOrderBy () {
+    setOrderBy((prevState) => prevState === 'asc' ? 'desc' : 'asc')
+  }
+
+  function handleChangeSearchTerm (e) {
+    setSearchTerm(e.target.value)
+  }
+
   return (
     <>
-      <Modal danger />
+      <Loader isLoading={isLoading} />
 
       <S.InputSearchContainer>
-        <input type="text" name="search" placeholder='Search contact by name...'/>
+        <S.InputSearch
+          value={searchTerm}
+          type="text"
+          name="search"
+          placeholder='Search contact by name...'
+          onChange={handleChangeSearchTerm}
+        />
       </S.InputSearchContainer>
 
       <S.Container>
-        <S.ContactsNumber>3 contacts</S.ContactsNumber>
-        <Link to="/new">New contact</Link>
+        <S.ContactsNumber>
+          {filteredContacts.length}
+          {filteredContacts.length === 1 ? ' contact' : ' contacts'}
+        </S.ContactsNumber>
 
+        <Link to="/new">New contact</Link>
       </S.Container>
 
       <S.ListContainer>
-        <S.ListHeader>
-          <S.OrderButton type="button">
+        {filteredContacts.length > 0 && (
+          <S.OrderButton type="button" onClick={handleToggleOrderBy}>
             Name
-            <S.ArrowIcon src={arrow} alt="Arrow" />
+            <S.ArrowIcon src={arrow} alt="Arrow" orderBy={orderBy} />
           </S.OrderButton>
-        </S.ListHeader>
+        )}
 
-        <CardsList />
-
+        <CardsList contacts={filteredContacts} />
       </S.ListContainer>
     </>
   )
