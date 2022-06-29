b73e38e83fd3d81d22801c792dcb1e7a457e3b16
Mateus Victor <mateusvictor.dev@gmail.com>
2022-06-29 14:55:38 -0300

    feat: add custom error

---
+++ src/errors/APIError.js
@@ -0,0 +1,10 @@
+export default class APIError extends Error {
+  constructor (response, body) {
+    super()
+
+    this.name = 'APIError'
+    this.response = response
+    this.body = body
+    this.message = body?.error || `${response.status} - ${response.statusText}`
+  }
+}
