93c3099071cfd4a052bf33f2603f045b91da4385
Mateus Victor <mateusvictor.dev@gmail.com>
2022-06-29 15:01:54 -0300

    feat: add validation before parsing response body

--- src/services/utils/HttpClient.js
+++ src/services/utils/HttpClient.js
@@ -1,16 +1,28 @@
 import delay from '../../utils/delay'
-
+import APIError from '../../errors/APIError'
 class HttpClient {
   constructor (baseURL) {
     this.baseURL = baseURL
   }
 
   async get (path) {
-    const response = await fetch(`${this.baseURL}${path}`)
-
     await delay(500)
 
-    return await response.json()
+    const response = await fetch(`${this.baseURL}${path}`)
+
+    let body = null
+
+    const contentType = response.headers.get('content-type')
+
+    if (contentType.includes('application/json')) {
+      body = await response.json()
+    }
+
+    if (response.ok) {
+      return body
+    }
+
+    throw new APIError(response, body)
   }
 }
 
