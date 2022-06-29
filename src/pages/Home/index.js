26a31d07d76b63f0b27e3a6b52fe016f354414c6
Mateus Victor <mateusvictor.dev@gmail.com>
2022-06-29 18:16:19 -0300

    feat: add conditional rendering based on hasError state

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,10 +1,13 @@
-import { useState, useEffect, useMemo } from 'react'
+import { useState, useEffect, useMemo, useCallback } from 'react'
 import { Link } from 'react-router-dom'
 
 import arrow from '../../assets/icons/arrow.svg'
+import sad from '../../assets/images/sad.svg'
 
-import { CardsList } from '../../components/CardsList'
+import { ContactsList } from '../../components/ContactsList'
 import { Loader } from '../../components/Loader'
+import { Button } from '../../components/Button'
+
 import ContactsService from '../../services/ContactsService'
 
 import * as S from './styles'
@@ -14,6 +17,7 @@ function Home () {
   const [orderBy, setOrderBy] = useState('asc')
   const [searchTerm, setSearchTerm] = useState('')
   const [isLoading, setIsLoading] = useState(true)
+  const [hasError, setHasError] = useState(false)
 
   const filteredContacts = useMemo(() => {
     return contacts.filter(contact => (
@@ -21,23 +25,26 @@ function Home () {
     ))
   }, [contacts, searchTerm])
 
-  useEffect(() => {
-    async function getContacts () {
-      try {
-        setIsLoading(true)
+  const shouldRenderContactsList = filteredContacts.length > 0 && !hasError
 
-        const contactsList = await ContactsService.listContacts(orderBy)
+  const loadContacts = useCallback(async () => {
+    try {
+      setIsLoading(true)
 
-        setContacts(contactsList)
-      } catch (error) {
-        throw new Error()
-      } finally {
-        setIsLoading(false)
-      }
+      const contactsList = await ContactsService.listContacts(orderBy)
+
+      setHasError(false)
+      setContacts(contactsList)
+    } catch {
+      setHasError(true)
+    } finally {
+      setIsLoading(false)
     }
-
-    getContacts()
   }, [orderBy])
+
+  useEffect(() => {
+    loadContacts()
+  }, [loadContacts])
 
   function handleToggleOrderBy () {
     setOrderBy((prevState) => prevState === 'asc' ? 'desc' : 'asc')
@@ -45,6 +52,10 @@ function Home () {
 
   function handleChangeSearchTerm (e) {
     setSearchTerm(e.target.value)
+  }
+
+  function handleTryAgain () {
+    loadContacts()
   }
 
   return (
@@ -61,24 +72,41 @@ function Home () {
         />
       </S.InputSearchContainer>
 
-      <S.Container>
-        <S.ContactsNumber>
-          {filteredContacts.length}
-          {filteredContacts.length === 1 ? ' contact' : ' contacts'}
-        </S.ContactsNumber>
+      <S.Container hasError={hasError}>
+        {!hasError && (
+          <S.ContactsNumber>
+            {filteredContacts.length}
+            {filteredContacts.length === 1 ? ' contact' : ' contacts'}
+          </S.ContactsNumber>
+        )}
 
         <Link to="/new">New contact</Link>
       </S.Container>
 
       <S.ListContainer>
-        {filteredContacts.length > 0 && (
-          <S.OrderButton type="button" onClick={handleToggleOrderBy}>
-            Name
-            <S.ArrowIcon src={arrow} alt="Arrow" orderBy={orderBy} />
-          </S.OrderButton>
+        {hasError && (
+          <S.ErrorContainer>
+            <S.ErrorIcon src={sad} />
+
+            <S.ErrorDetails>
+              <S.ErrorMessage>There was an error getting the contacts.</S.ErrorMessage>
+
+              <Button onClick={handleTryAgain}>Try again</Button>
+            </S.ErrorDetails>
+          </S.ErrorContainer>
         )}
 
-        <CardsList contacts={filteredContacts} />
+        {shouldRenderContactsList && (
+          <>
+            <S.OrderButton type="button" onClick={handleToggleOrderBy}>
+              Name
+              <S.ArrowIcon src={arrow} alt="Arrow" orderBy={orderBy} />
+            </S.OrderButton>
+
+            <ContactsList contacts={filteredContacts} />
+          </>
+        )}
+
       </S.ListContainer>
     </>
   )
