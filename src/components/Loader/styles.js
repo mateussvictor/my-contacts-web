d39a5c443583e68fc6f6220684ef0155f7df0385
Mateus Victor <mateusvictor.dev@gmail.com>
2022-07-07 16:37:06 -0300

    fix: set position fixed on overlay

--- src/components/Loader/styles.js
+++ src/components/Loader/styles.js
@@ -35,7 +35,7 @@ const round = keyframes`
 `
 
 export const Overlay = styled.div`
-  position: absolute;
+  position: fixed;
   width: 100%;
   height: 100%;
   top: 0;
@@ -43,23 +43,6 @@ export const Overlay = styled.div`
   background: rgba(246, 245, 252, 0.7);
   display: grid;
   place-items: center;
-
-  /* .loader {
-    color: ${({ theme }) => theme.colors.primary.normal};
-    font-size: 90px;
-    text-indent: -9999em;
-    overflow: hidden;
-    width: 1em;
-    height: 1em;
-    border-radius: 50%;
-    margin: 72px auto;
-    position: relative;
-    -webkit-transform: translateZ(0);
-    -ms-transform: translateZ(0);
-    transform: translateZ(0);
-    -webkit-animation: ${load} 1.7s infinite ease, ${round} 1.7s infinite ease;
-    animation: ${load} 1.7s infinite ease, ${round} 1.7s infinite ease;
-  } */
 `
 
 export const Loader = styled.div`
