9045f60f53aeffe234a8dd5645923dad8448a9df
Mateus Victor <mateusvictor.dev@gmail.com>
2022-08-03 17:34:11 +0100

    feat: set conditional rendering

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -3,6 +3,8 @@ import { Link } from 'react-router-dom'
 
 import arrow from '../../assets/icons/arrow.svg'
 import sad from '../../assets/images/sad.svg'
+import emptyBox from '../../assets/images/empty-box.svg'
+import magnifier from '../../assets/images/magnifier-question.svg'
 
 import { ContactsList } from '../../components/ContactsList'
 import { Loader } from '../../components/Loader'
@@ -24,8 +26,6 @@ function Home () {
       contact.name.toLowerCase().includes(searchTerm.toLowerCase())
     ))
   }, [contacts, searchTerm])
-
-  const shouldRenderContactsList = filteredContacts.length > 0 && !hasError
 
   const loadContacts = useCallback(async () => {
     try {
@@ -62,18 +62,31 @@ function Home () {
     <>
       <Loader isLoading={isLoading} />
 
-      <S.InputSearchContainer>
-        <S.InputSearch
-          value={searchTerm}
-          type="text"
-          name="search"
-          placeholder='Search contact by name...'
-          onChange={handleChangeSearchTerm}
-        />
-      </S.InputSearchContainer>
+      {contacts.length > 0 && (
+        <S.InputSearchContainer>
+          <S.InputSearch
+            value={searchTerm}
+            type="text"
+            name="search"
+            placeholder='Search contact by name...'
+            onChange={handleChangeSearchTerm}
+          />
+        </S.InputSearchContainer>
+      )}
 
-      <S.Container hasError={hasError}>
-        {!hasError && (
+      <S.Container
+        hasError={hasError}
+        justifyContent={
+          hasError
+            ? 'flex-end'
+            : (
+                contacts.length > 0
+                  ? 'space-between'
+                  : 'center'
+              )
+          }
+      >
+        {(!hasError && contacts.length > 0) && (
           <S.ContactsNumber>
             {filteredContacts.length}
             {filteredContacts.length === 1 ? ' contact' : ' contacts'}
@@ -96,14 +109,37 @@ function Home () {
           </S.ErrorContainer>
         )}
 
-        {shouldRenderContactsList && (
+        {!hasError && (
           <>
-            <S.OrderButton type="button" onClick={handleToggleOrderBy}>
-              Name
-              <S.ArrowIcon src={arrow} alt="Arrow" orderBy={orderBy} />
-            </S.OrderButton>
+            {(contacts.length < 1 && !isLoading) && (
+              <S.EmptyListContainer>
+                <S.EmptyBox src={emptyBox} alt="Empty box" />
+                <p>
+                  You do not have any contacts registered yet.
+                  Click on the <strong>&quot;New contact&quot;</strong>
+                  button above to register your first one.
+                </p>
+              </S.EmptyListContainer>
+            )}
 
-            <ContactsList contacts={filteredContacts} />
+            {(contacts.length > 0 && filteredContacts.length < 1) && (
+              <S.SearchNotFoundContainer>
+                <img src={magnifier} alt="Magnifier" />
+
+                <span>Result not found for <strong>&quot;{searchTerm}&quot;</strong></span>
+              </S.SearchNotFoundContainer>
+            )}
+
+            {filteredContacts.length > 0 && (
+              <>
+                <S.OrderButton type="button" onClick={handleToggleOrderBy}>
+                  Name
+                  <S.ArrowIcon src={arrow} alt="Arrow" orderBy={orderBy} />
+                </S.OrderButton>
+
+                <ContactsList contacts={filteredContacts} />
+              </>
+            )}
           </>
         )}
 
--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -23,7 +23,7 @@ export const Container = styled.header`
   ${({ theme, hasError }) => css`
     margin-top: ${theme.spacings.medium};
     display: flex;
-    justify-content: ${hasError ? 'flex-end' : 'space-between'};
+    justify-content: ${({ justifyContent }) => justifyContent};
     align-items: center;
     border-bottom: 2px solid ${theme.colors.gray.light};
     padding-bottom: 1.6rem;
@@ -53,6 +53,36 @@ export const ContactsNumber = styled.strong``
 
 export const ListContainer = styled.section`
   margin-top: 3.2rem;
+`
+
+export const EmptyListContainer = styled.div`
+  display: inline-block;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  p {
+    color: ${({ theme }) => theme.colors.gray.normal};
+    text-align: center;
+    margin-top: 1.6rem;
+
+    strong {
+      color: ${({ theme }) => theme.colors.primary.normal};
+    }
+  }
+`
+
+export const EmptyBox = styled.img``
+
+export const SearchNotFoundContainer = styled.div`
+  display: flex;
+  align-items: flex-start;
+  gap: 2.4rem;
+
+  span {
+    color: ${({ theme }) => theme.colors.gray.normal};
+    word-break: break-word;
+  }
 `
 
 export const OrderButton = styled.button`
