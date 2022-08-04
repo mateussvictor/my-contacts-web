5edb8452d02fa4532483c90054394936be201a22
Mateus Victor <mateusvictor.dev@gmail.com>
2022-08-04 20:28:59 +0100

    feat: add createContact method

--- src/services/ContactsService.js
+++ src/services/ContactsService.js
@@ -9,6 +9,10 @@ class ContactsService {
   async listContacts (orderBy = 'asc') {
     return this.httpClient.get(`/contacts/?orderBy=${orderBy}`)
   }
+
+  async createContact (contact) {
+    return this.httpClient.post('/contacts', contact)
+  }
 }
 
 export default new ContactsService()
