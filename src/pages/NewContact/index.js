55722280785aef8a2cd394b4379101487275950b
Mateus Victor <mateusvictor.dev@gmail.com>
2022-08-24 20:02:24 +0100

    feat: add handleSubmit function

--- src/pages/NewContact/index.js
+++ src/pages/NewContact/index.js
@@ -1,13 +1,33 @@
 
 import { PageHeader } from '../../components/PageHeader'
 import { ContactForm } from '../../components/ContactForm'
+import ContactsService from '../../services/ContactsService'
 
 function NewContact () {
+  async function handleSubmit (formData) {
+    try {
+      const contact = {
+        name: formData.name,
+        email: formData.email,
+        phone: formData.phone,
+        category_id: formData.categoryId
+      }
+
+      const response = await ContactsService.createContact(contact)
+      console.log(response)
+    } catch (error) {
+      alert('An error occurred while creating')
+    }
+  }
+
   return (
     <>
       <PageHeader title="New Contact" />
 
-      <ContactForm buttonLabel="Save Contact" />
+      <ContactForm
+        onSubmit={handleSubmit}
+        buttonLabel="Save Contact"
+      />
     </>
   )
 }
