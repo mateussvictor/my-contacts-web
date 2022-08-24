58ffbbd2ad7993f87b2778d6f41b18bf83398146
Mateus Victor <mateusvictor.dev@gmail.com>
2022-08-24 20:02:00 +0100

    fix: send request body

--- src/services/ContactsService.js
+++ src/services/ContactsService.js
@@ -11,7 +11,7 @@ class ContactsService {
   }
 
   async createContact (contact) {
-    return this.httpClient.post('/contacts', contact)
+    return this.httpClient.post('/contacts', { body: contact })
   }
 }
 
