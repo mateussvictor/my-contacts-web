80f3f392c77e21755bfd2dcaf3f7cb08bd3101df
Mateus Victor <mateusvictor.dev@gmail.com>
2022-09-15 22:03:20 +0100

    feat: add toast util function

---
+++ src/utils/toast.js
@@ -0,0 +1,7 @@
+import EventManager from '../lib/EventManager'
+
+export const toastEventManager = new EventManager()
+
+export default function toast ({ type, text, duration }) {
+  toastEventManager.emit('addtoast', { type, text, duration })
+}
