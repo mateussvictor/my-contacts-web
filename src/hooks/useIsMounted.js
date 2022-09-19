03c5fdb918bbd2b0ab4e6f0f794b16f41080f66e
Mateus Victor <mateusvictor.dev@gmail.com>
2022-09-19 17:24:42 +0100

    chore: add useIsMounted hook

---
+++ src/hooks/useIsMounted.js
@@ -0,0 +1,19 @@
+import { useRef, useEffect, useCallback } from 'react'
+
+function useIsMounted () {
+  const isMounted = useRef(false)
+
+  useEffect(() => {
+    isMounted.current = true
+
+    return () => {
+      isMounted.current = false
+    }
+  }, [])
+
+  const getIsMounted = useCallback(() => isMounted.current)
+
+  return getIsMounted
+}
+
+export { useIsMounted }
