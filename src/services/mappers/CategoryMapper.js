43f2ac347e220aeec8883366b2b8ee0dbdb664cc
Mateus Victor <mateusvictor.dev@gmail.com>
2022-11-02 16:53:50 +0000

    feat: create CategoryMapper

---
+++ src/services/mappers/CategoryMapper.js
@@ -0,0 +1,10 @@
+class CategoryMapper {
+  toDomain (persistenceCategory) {
+    return {
+      id: persistenceCategory.id,
+      name: persistenceCategory.name
+    }
+  }
+}
+
+export default new CategoryMapper()
