58d2f244e8ce543f397d34b936915cdcb724dc76
mateus.victor <mateusvictor.dev@gmail.com>
2023-01-03 22:13:25 +0000

    refactor: apply hooks to separate UI/logic on form

--- src/components/ContactForm/index.js
+++ src/components/ContactForm/index.js
@@ -1,102 +1,32 @@
-import { useState, useEffect, forwardRef, useImperativeHandle } from 'react'
 import PropTypes from 'prop-types'
-
-import useErrors from '../../hooks/useErrors'
-
-import isValidEmail from '../../utils/isValidEmail'
-import formatPhone from '../../utils/formatPhone'
-import CategoriesService from '../../services/CategoriesService'
+import { forwardRef } from 'react'
 
 import { FormGroup } from '../FormGroup'
 import { Input } from '../Input'
 import { Select } from '../Select'
 import { Button } from '../Button'
 
-import { useSafeAsyncState } from '../../hooks/useSafeAsyncState'
 import * as S from './styles'
 
+import useContactForm from './useContactForm'
+
 const ContactForm = forwardRef(({ buttonLabel, onSubmit }, ref) => {
-  const [name, setName] = useState('')
-  const [email, setEmail] = useState('')
-  const [phone, setPhone] = useState('')
-  const [categoryId, setCategoryId] = useState('')
-  const [categories, setCategories] = useSafeAsyncState([])
-  const [isLoadingCategories, setIsLoadingCategories] = useSafeAsyncState(true)
-  const [isSubmitting, setIsSubmitting] = useState(false)
-
   const {
-    setError,
-    removeError,
+    handleSubmit,
     getErrorMessageByFieldName,
-    errors
-  } = useErrors()
-
-  const isFormValid = (name && errors.length === 0)
-
-  useImperativeHandle(ref, () => ({
-    setFieldsValues: (contact) => {
-      setName(contact.name ?? '')
-      setEmail(contact.email ?? '')
-      setPhone(formatPhone(contact.phone ?? ''))
-      setCategoryId(contact.category.id ?? '')
-    },
-    resetFields: () => {
-      setName('')
-      setEmail('')
-      setPhone('')
-      setCategoryId('')
-    }
-  }), [])
-
-  useEffect(() => {
-    async function loadCategories () {
-      try {
-        const categoriesList = await CategoriesService.listCategories()
-
-        setCategories(categoriesList)
-      } catch {} finally {
-        setIsLoadingCategories(false)
-      }
-    }
-
-    loadCategories()
-  }, [setCategories, setIsLoadingCategories])
-
-  function handleNameChange (e) {
-    setName(e.target.value)
-
-    if (!e.target.value) {
-      setError({ field: 'name', message: 'Name is required.' })
-    } else {
-      removeError('name')
-    }
-  }
-
-  function handleEmailChange (e) {
-    setEmail(e.target.value)
-
-    if (e.target.value && !isValidEmail(e.target.value)) {
-      setError({ field: 'email', message: 'Invalid email.' })
-    } else {
-      removeError('email')
-    }
-  }
-
-  async function handleSubmit (e) {
-    e.preventDefault()
-
-    setIsSubmitting(true)
-
-    await onSubmit({
-      name, email, phone, categoryId
-    })
-
-    setIsSubmitting(false)
-  }
-
-  function handlePhoneChange (e) {
-    setPhone(formatPhone(e.target.value))
-  }
+    handleNameChange,
+    name,
+    isSubmitting,
+    handleEmailChange,
+    email,
+    handlePhoneChange,
+    phone,
+    isLoadingCategories,
+    categoryId,
+    setCategoryId,
+    categories,
+    isFormValid
+  } = useContactForm(onSubmit, ref)
 
   return (
     <S.Form onSubmit={handleSubmit} noValidate>
---
+++ src/components/ContactForm/useContactForm.js
@@ -0,0 +1,113 @@
+
+import { useState, useEffect, useImperativeHandle } from 'react'
+
+import isValidEmail from '../../utils/isValidEmail'
+import formatPhone from '../../utils/formatPhone'
+import CategoriesService from '../../services/CategoriesService'
+
+import useErrors from '../../hooks/useErrors'
+import useSafeAsyncState from '../../hooks/useSafeAsyncState'
+
+export default function useContactForm (onSubmit, ref) {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [phone, setPhone] = useState('')
+  const [categoryId, setCategoryId] = useState('')
+  const [categories, setCategories] = useSafeAsyncState([])
+  const [isLoadingCategories, setIsLoadingCategories] = useSafeAsyncState(true)
+  const [isSubmitting, setIsSubmitting] = useState(false)
+
+  const {
+    setError,
+    removeError,
+    getErrorMessageByFieldName,
+    errors
+  } = useErrors()
+
+  const isFormValid = (name && errors.length === 0)
+
+  useImperativeHandle(ref, () => ({
+    setFieldsValues: (contact) => {
+      setName(contact.name ?? '')
+      setEmail(contact.email ?? '')
+      setPhone(formatPhone(contact.phone ?? ''))
+      setCategoryId(contact.category.id ?? '')
+    },
+    resetFields: () => {
+      setName('')
+      setEmail('')
+      setPhone('')
+      setCategoryId('')
+    }
+  }), [])
+
+  useEffect(() => {
+    async function loadCategories () {
+      try {
+        const categoriesList = await CategoriesService.listCategories()
+
+        setCategories(categoriesList)
+      } catch {} finally {
+        setIsLoadingCategories(false)
+      }
+    }
+
+    loadCategories()
+  }, [setCategories, setIsLoadingCategories])
+
+  function handleNameChange (e) {
+    setName(e.target.value)
+
+    if (!e.target.value) {
+      setError({ field: 'name', message: 'Name is required.' })
+    } else {
+      removeError('name')
+    }
+  }
+
+  function handleEmailChange (e) {
+    setEmail(e.target.value)
+
+    if (e.target.value && !isValidEmail(e.target.value)) {
+      setError({ field: 'email', message: 'Invalid email.' })
+    } else {
+      removeError('email')
+    }
+  }
+
+  function handlePhoneChange (e) {
+    setPhone(formatPhone(e.target.value))
+  }
+
+  async function handleSubmit (e) {
+    e.preventDefault()
+
+    setIsSubmitting(true)
+
+    await onSubmit({
+      name,
+      email,
+      phone,
+      categoryId
+    })
+
+    setIsSubmitting(false)
+  }
+
+  return {
+    handleSubmit,
+    getErrorMessageByFieldName,
+    handleNameChange,
+    name,
+    isSubmitting,
+    handleEmailChange,
+    email,
+    handlePhoneChange,
+    phone,
+    isLoadingCategories,
+    categoryId,
+    setCategoryId,
+    categories,
+    isFormValid
+  }
+}
