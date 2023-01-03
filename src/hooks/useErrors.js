327101c2b6604e1e519ab5cc06cc9161c3c0faa6
mateus.victor <mateusvictor.dev@gmail.com>
2023-01-03 22:16:28 +0000

    refactor: add useCallback on custom hook functions

--- src/hooks/useErrors.js
+++ src/hooks/useErrors.js
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+import { useState, useCallback } from 'react'
 
 export default function useErrors () {
   const [errors, setErrors] = useState([])
 
-  function setError ({ field, message }) {
+  const setError = useCallback(({ field, message }) => {
     const errorAlreadyExists = errors.find(error => error.field === field)
 
     if (errorAlreadyExists) return
@@ -12,17 +12,17 @@ export default function useErrors () {
       ...prevState,
       { field, message }
     ])
-  }
+  }, [errors])
 
-  function removeError (fieldName) {
+  const removeError = useCallback((fieldName) => {
     setErrors(prevState => prevState.filter(
       error => error.field !== fieldName
     ))
-  }
+  }, [])
 
-  function getErrorMessageByFieldName (fieldName) {
-    return errors.find(error => error.field === fieldName)?.message
-  }
+  const getErrorMessageByFieldName = useCallback((fieldName) => (
+    errors.find(error => error.field === fieldName)?.message
+  ), [errors])
 
   return {
     setError,
